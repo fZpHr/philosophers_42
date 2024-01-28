eefae554298bce9c6a5c5fe1d9ffd4b49e7ea52d
Zeph <92046611+fZpHr@users.noreply.github.com>
2024-01-28 17:50:01 +0100

    Update monitor_death.c

--- philo/srcs/algo/monitor_death.c
+++ philo/srcs/algo/monitor_death.c
@@ -14,6 +14,9 @@
 
 void	to_late(t_philo *p, int i)
 {
+	pthread_mutex_lock(&p->finishm);
+	p->finish = 1;
+	pthread_mutex_unlock(&p->finishm);
 	pthread_mutex_unlock(&p->meal);
 	pthread_mutex_lock(&p->meal);
 	pthread_mutex_lock(&p->print);
@@ -21,9 +24,6 @@ void	to_late(t_philo *p, int i)
 		(get_current_time() - p->last_meal[i]), i);
 	pthread_mutex_unlock(&p->print);
 	pthread_mutex_unlock(&p->meal);
-	pthread_mutex_lock(&p->finishm);
-	p->finish = 1;
-	pthread_mutex_unlock(&p->finishm);
 }
 
 void	monitor_thread_death(t_philo *p)
